04ab362f5c2a0f86fe636542c56b7d95de01852e
Ian Ellis <ellisiana@gmail.com>
2023-07-19 07:13:02 +0100

    just use python

--- GitlabToConfluence.sh
+++
@@ -1,3 +0,0 @@
-curl "https://gitlab.com/api/v4/projects/46187091/repository/files/README.md?private_token=$GITLAB_TOKEN&ref=main"|jq .content|cut -d\" -f2|base64 --decode> README.md
-
-python atlassian_python_api_code.py
